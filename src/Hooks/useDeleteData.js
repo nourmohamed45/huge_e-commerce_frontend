4746e5bb0f8c36e7147b60d99eabd5e8efd9829c
nourmohamed45 <nourelagwany45@gmail.com>
2024-07-11 17:48:03 +0300

    fix issue

--- src/Hooks/useDeleteData.js
+++ src/Hooks/useDeleteData.js
@@ -1,14 +1,13 @@
-
-import baseUrl from "../Api/baseURL"
+import baseUrl from "../Api/baseURL";
 
 const useDeleteData = async (url, params) => {
   const config = {
     headers: {
-      Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2OGQwZDM3NzI4NWEzZTc4OWZlNTMyMCIsImlhdCI6MTcyMDY1MTI2MywiZXhwIjoxNzI4NDI3MjYzfQ.DTuWmz2k6xsmS29k5wm_unaek8j2aAUYDElhOdXBsfE`,
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
-  }
+  };
   const response = await baseUrl.delete(url, params, config);
   return response.data;
-}
+};
 
 export default useDeleteData;
